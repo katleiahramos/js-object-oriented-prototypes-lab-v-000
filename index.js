afad4494fe1c639743091e2a37de723359666759
Katleiah Ramos <katleiahramos@gmail.com>
2018-07-24 15:49:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ function BoardMember(name, homeState, training){
   // this.veto = function(){
   //   return 'No, I must disagree'
   // }
-  this.approve = function(){
-    return 'You can do that!'
-  }
+  // this.approve = function(){
+  //   return 'You can do that!'
+  // }
   this.doCharity = function(){
     return "I like to help people."
   }
