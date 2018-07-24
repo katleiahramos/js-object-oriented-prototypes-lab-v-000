ac24b08ce10ffac5817d96c8d615b32955e0c540
Katleiah Ramos <katleiahramos@gmail.com>
2018-07-24 15:51:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ function BoardMember(name, homeState, training){
   //   return "I like to help people."
   // }
 
-  this.releasePressStatement = function(){
-    return "You will see great things from Scuber.";
-  }
-
-  this.sayHi = function(){
-    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
-  }
+  // this.releasePressStatement = function(){
+  //   return "You will see great things from Scuber.";
+  // }
+  //
+  // this.sayHi = function(){
+  //   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
+  // }
 }
 
 BoardMember.prototype.veto = function(){
