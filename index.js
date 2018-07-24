3145e8a3e263cd4cd6fc15c351204da80f68eda4
Katleiah Ramos <katleiahramos@gmail.com>
2018-07-24 15:49:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,3 +25,7 @@ function BoardMember(name, homeState, training){
 BoardMember.prototype.veto = function(){
   return 'No, I must disagree'
 }
+
+BoardMember.prototype.approve = function () {
+  return 'You can do that!'
+};
